359da89972aea766142b0fa2408f9cfe490c05bb
Ampo <sujustin20@gmail.com>
2022-11-24 04:15:14 +0000

    Added array header file

--- Array/Array.h
+++ Array/Array.h
@@ -1,8 +1,25 @@
 #ifndef
 #define
 
+
 class Array
 {
+   public:
+	Array();
+	Array(int capacity);
+	~Array();
+	void insert(int value, int index);
+	void removeAt(int index);
+	int valueAt(int index);
+	void insertion_sort();
+	void print();
+	int size();
+	int capacity();
+    private:
+	int* array;
+	int size;
+	int capacity; 
+	
 
 }
 
